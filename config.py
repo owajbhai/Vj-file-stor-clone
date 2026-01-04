f33aabd3c1bb91bc8870bd14a58dd48e2c03ddc2
owajbhai <owajshaikhrcm@gmail.com>
2026-01-04 15:49:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ API_HASH = environ.get("API_HASH", "de904be2b4cd4efe2ea728ded17ca77d")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7107488581:AAGFd6unhkuBrchp0kgDyjMczy24uFY06B8")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1249672673').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "TGsmmShop_bot") # without @
 PORT = environ.get("PORT", "8080")
 
